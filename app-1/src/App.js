7164cc5c38fa266a3a4696ed6df7e8a02d89422f
Zach King <zacheking10@gmail.com>
2021-10-05 16:46:56 -0600

    app-1 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+
+const App = () => {
+  const [state, setState] = useState("")
+  console.log(state);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={(e) => setState(e.target.value)}/>
+      <p>{state}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
